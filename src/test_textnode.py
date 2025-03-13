9b7257400a6a64671f1407ccbd1cf5f7d014c216
krotla <kkornel@o2.pl>
2025-03-13 23:54:25 +0100

    feat: Add function to convert from TextNode to HtmlNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from main import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -26,13 +27,19 @@ class TestTextNode(unittest.TestCase):
 
     def test_neq2(self):
         node = TextNode("This is a text node2", TextType.ITALIC, None)
-        node2 = TextNode("This is a text node2", TextType.NORMAL, None)
+        node2 = TextNode("This is a text node2", TextType.TEXT, None)
         self.assertNotEqual(node, node2)
 
     def test_neq3(self):
         node = TextNode("This is a text node", TextType.LINK, "www.boot.dev")
         node2 = TextNode("This is a text node", TextType.LINK, "www.boot.dev.com")
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
