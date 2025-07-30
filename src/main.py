450ea6b9e5ce15416540570dd60eefdb2a0be4d4
krotla <kkornel@o2.pl>
2025-07-30 14:22:37 +0200

    feat: Split TEXT nodes based on delimiter and marked it as provided TextType

--- src/main.py
+++ src/main.py
@@ -22,4 +22,25 @@ def text_node_to_html_node(text_node):
         case _:
             raise Exception("This TextType does not exist!") 
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    splitted = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            splitted.append(old_node)
+            continue
+        splited_texts = old_node.text.split(delimiter)
+        if len(splited_texts) % 2 == 0:
+            raise Exception(f"Invalid Markdown syntax! Odd number of {delimiter} delimiters.")
+        for i, text in enumerate(splited_texts):               
+            if i % 2 == 0:
+                if text == '':
+                    continue
+                new_node = TextNode(text, TextType.TEXT)
+            else:
+                new_node = TextNode(text, text_type)
+            splitted.append(new_node)
+
+    return splitted
+
+
 main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from main import text_node_to_html_node
+from main import text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -40,6 +40,18 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node, node2], "`", TextType.CODE)
+        correct_nodes = [
+            TextNode("This is a text node", TextType.BOLD),
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, correct_nodes)
 
 
 if __name__ == "__main__":
